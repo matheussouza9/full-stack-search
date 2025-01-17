8df1e34225bdc793b437b17959b1531658bc175f
Matheus Souza <matheussouzadecarvalho@gmail.com>
2025-01-17 16:50:18 +0000

    make use of /search endpoint by the client application and fix the 'clear search' button

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -1,38 +1,59 @@
-import { useState, type ChangeEvent } from 'react';
+import { useEffect, useState } from 'react';
 import { getCodeSandboxHost } from "@codesandbox/utils";
 
 type Hotel = { _id: string, chain_name: string; hotel_name: string; city: string, country: string };
+type City = { _id: string; name: string };
+type Country = { _id: string; name: string };
+
+type AccommodationResult = {
+  hotels: Hotel[];
+  cities: City[];
+  countries: Country[];
+}
 
 const codeSandboxHost = getCodeSandboxHost(3001)
 const API_URL = codeSandboxHost ? `https://${codeSandboxHost}` : 'http://localhost:3001'
 
-const fetchAndFilterHotels = async (value: string) => {
-  const hotelsData = await fetch(`${API_URL}/hotels`);
-  const hotels = (await hotelsData.json()) as Hotel[];
-  return hotels.filter(
-    ({ chain_name, hotel_name, city, country }) =>
-      chain_name.toLowerCase().includes(value.toLowerCase()) ||
-      hotel_name.toLowerCase().includes(value.toLowerCase()) ||
-      city.toLowerCase().includes(value.toLowerCase()) ||
-      country.toLowerCase().includes(value.toLowerCase())
-  );
+const fetchAccomodationsData = async (value: string) => {
+  const data = await fetch(`${API_URL}/search?term=${value}`);
+  const accomodationResult = (await data.json()) as AccommodationResult;
+  return accomodationResult;
 }
 
 function App() {
-  const [hotels, setHotels] = useState<Hotel[]>([]);
+  const [search, setSearch] = useState('');
   const [showClearBtn, setShowClearBtn] = useState(false);
 
-  const fetchData = async (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.value === '') {
+  const [loadingData, setLoadingData] = useState(false);
+
+  const [hotels, setHotels] = useState<Hotel[]>([]);
+  const [cities, setCities] = useState<City[]>([]);
+  const [countries, setCountries] = useState<Country[]>([]);
+
+  const fetchData = async (term: string) => {
+    if (term.length < 3) {
       setHotels([]);
+      setCities([]);
+      setCountries([]);
       setShowClearBtn(false);
       return;
     }
 
-    const filteredHotels = await fetchAndFilterHotels(event.target.value)
-    setShowClearBtn(true);
-    setHotels(filteredHotels);
+    try {
+      setLoadingData(true);
+      const accomodationData = await fetchAccomodationsData(term);
+      setShowClearBtn(true);
+      setHotels(accomodationData.hotels);
+      setCities(accomodationData.cities);
+      setCountries(accomodationData.countries);
+    } finally {
+      setLoadingData(false);
+    }
   };
+
+  useEffect(() => {
+    fetchData(search);
+  }, [search]);
 
   return (
     <div className="App">
@@ -41,20 +62,21 @@ function App() {
           <div className="col-md-6">
             <div className="dropdown">
               <div className="form">
-                <i className="fa fa-search"></i>
+                <i className={`fa input-prefix-icon ${loadingData ? 'fa-spinner fa-spin' : 'fa-search'}`}></i>
                 <input
                   type="text"
                   className="form-control form-input"
                   placeholder="Search accommodation..."
-                  onChange={fetchData}
+                  value={search}
+                  onChange={e => setSearch(e.target.value)}
                 />
                 {showClearBtn && (
-                  <span className="left-pan">
+                  <span className="left-pan pe-auto" onClick={() => setSearch('')}>
                     <i className="fa fa-close"></i>
                   </span>
                 )}
               </div>
-              {!!hotels.length && (
+              {showClearBtn && (
                 <div className="search-dropdown-menu dropdown-menu w-100 show p-2">
                   <h2>Hotels</h2>
                   {hotels.length ? hotels.map((hotel, index) => (
@@ -66,10 +88,28 @@ function App() {
                       <hr className="divider" />
                     </li>
                   )) : <p>No hotels matched</p>}
+
                   <h2>Countries</h2>
-                  <p>No countries matched</p>
+                  {countries.length ? countries.map((country, index) => (
+                    <li key={index}>
+                      <a href={`/country/${country._id}`} className="dropdown-item">
+                        <i className="fa fa-flag mr-2"></i>
+                        {country.name}
+                      </a>
+                      <hr className="divider" />
+                    </li>
+                  )) : <p>No countries matched</p>}
+
                   <h2>Cities</h2>
-                  <p>No cities matched</p>
+                  {cities.length ? cities.map((city, index) => (
+                    <li key={index}>
+                      <a href={`/city/${city._id}`} className="dropdown-item">
+                        <i className="fa fa-map-marker mr-2"></i>
+                        {city.name}
+                      </a>
+                      <hr className="divider" />
+                    </li>
+                  )) : <p>No cities matched</p>}
                 </div>
               )}
             </div>
